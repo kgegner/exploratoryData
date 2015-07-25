6e1f810196b01a3c1d45d55c578b50d7a68db8f1
kgegner <gegner2@illinois.edu>
2015-07-25 09:32:39 -0600

    update init

--- assign2/init.R
+++ assign2/init.R
@@ -1,6 +1,22 @@
 init <- function() {
-  # Run first thing, sets up workspace and required variables/objects
+  
+  # Run first thing, sets up workspace and required functions/variables/objects
+  
+  library(dplyr)
+  library(ggplot2)
+  library(stringr)
+
+  source("makeDataCache.R")
+  source("plot1.R")
+  source("plot2.R")
+  source("plot3.R")
+  source("plot4.R")
+  source("plot5.R")
+  source("plot6.R")
   
   # Create dataCache object to store NEI and SCC data
   dataCache <<- makeDataCache()
+  
+  NEI <<- tbl_df(readRDS("../data/exdata-data-NEI_data/summarySCC_PM25.rds"))
+  SCC <<- tbl_df(readRDS("../data/exdata-data-NEI_data/Source_Classification_Code.rds"))
 }
